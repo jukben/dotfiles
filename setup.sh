fc3c3948e7a6e09b04c461d4e80a8d8925b59343
Jakub Beneš <jukben@gmail.com>
2020-05-07 23:52:47 +0200

    Update setup script in order to hide desktop icons

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Find
 # For VIM Plugin
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
+# Hide Desktop icons
+defaults write com.apple.finder CreateDesktop false; killall Finder
