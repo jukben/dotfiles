89f036b9e533b6e5d318d309c7fdf0a197b3e80f
Jakub Benes <jukben@gmail.com>
2020-10-24 11:05:17 +0200

    Updated by Gbck

--- setup.sh
+++ setup.sh
@@ -6,5 +6,3 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Find
 # For VIM Plugin
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
-# Hide Desktop icons
-defaults write com.apple.finder CreateDesktop false; killall Finder
